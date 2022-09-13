84ec1ccf11fe846b313bbfa971c0ce4ae4ea2a12
Fabrizio-2025 <fabriziobc2020@outlook.com>
2022-09-12 20:06:59 -0500

    feat:first commit, cv website.

---
+++ stuff/nav.js
@@ -0,0 +1,51 @@
+var navbar = document.getElementById("navbar");
+var sidenavbar = document.getElementById("side-nav-options");
+var buttonCloseSideNavbar = document.getElementById("close-button");
+var url = "";
+
+function changeNavColorOnScroll() {
+  var pixelsGoingDown = window.scrollY;
+  const sizeOfNavbar = navbar.clientHeight;
+  if (pixelsGoingDown > sizeOfNavbar) {
+    navbar.style.background = "black";
+    navbar.style.transition = "all 0.1s ease-in";
+  }
+  else {
+    navbar.style.background = "#0a192f";
+    navbar.style.transition = "all 0.1s ease-in";
+  }
+}
+
+function showNavOptions() {
+  if (sidenavbar.clientWidth == 0) {
+    buttonCloseSideNavbar.style.visibility = "visible";
+    sidenavbar.style.width = document.documentElement.clientWidth * 0.3+ "px";
+  }
+  console.log("Clicked!");
+}
+
+function closeNavOptions() {
+  if (sidenavbar.clientWidth != 0) {
+    buttonCloseSideNavbar.style.visibility = "hidden";
+    sidenavbar.style.width = 0 + "px"; 
+  }
+  console.log("Clicked!");
+}
+
+function goToCurriculumPdf() {
+  window.open("", "_blank");
+}
+
+function main() {
+
+  window.onscroll = () => {
+    changeNavColorOnScroll();
+  }
+
+
+
+}
+
+
+main();
+
